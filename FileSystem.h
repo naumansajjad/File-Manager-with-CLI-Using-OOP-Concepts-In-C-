b7aa3b1b4ac458ca512e99f33ebe57153bf0eecf
Syed Nauman <nauman.sajjad3393@gmail.com>
2024-06-18 19:05:43 +0500

    Header Files Added

---
+++ FileSystem.h
@@ -0,0 +1,235 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <filesystem>
+using namespace std;
+namespace fs = filesystem;
+
+class FileSystem {
+    private:
+        void readfile(const string &filename);
+        bool makedirectory(const string &directoryname);
+        bool changedirectory(const string &directoryname);
+        bool copyfile(const string &source, const string &destination);
+        bool renamefile(const string &file, const string &newfile);
+        bool deletefile(const string &filename);
+        void writetofile(const string &filename, const string &content);
+        bool findfile(const string &filename);
+        void listdirectory(const string &directory);
+        void movetoparentdirectory(const string &directory);
+
+    public:
+        void read_file(const string &filename){
+            readfile(filename);
+        }
+        void make_directory(const string &directoryname){
+            makedirectory(directoryname);
+        }
+        void change_directory(const string &directoryname){
+            changedirectory(directoryname);
+        }
+        void copy_file(const string &sourcefile, const string &destinationfile){
+            copyfile(sourcefile, destinationfile);
+        }
+        void rename_file(const string &file,const string &newfile){
+            renamefile(file,newfile);
+        }
+        void delete_file(const string &filename){
+            deletefile(filename);
+        }
+        void write_to_file(const string &filename, const string &content){
+            writetofile(filename,content);
+        }
+        void find_file(const string &filename){
+            findfile(filename);
+        }
+        void list_directory(const string &directory){
+            listdirectory(directory);
+        }
+        void move_to_parent_directory(const string &directory){
+            movetoparentdirectory(directory);
+        }
+}obj_FS;
+
+void FileSystem::readfile(const string &filename){
+    ifstream file(filename);
+    if(file.is_open()){
+        string line;
+        cout << "Content of file: " << endl;
+        while(!file.eof()){
+            getline(file, line);
+            cout << line << endl;
+        }
+        file.close();
+    }else{
+        cerr << "Error: Unable to open file " << filename << " for reading" << endl;
+    }
+}
+
+bool FileSystem::makedirectory(const string &directoryname){
+    try{
+        fs::path currentpath = fs::current_path();
+        fs::path newpath = currentpath / directoryname;
+        if (fs::exists(newpath) && fs::is_directory(newpath)){
+            cerr << "Error: Directory " << directoryname << " already exists!" << endl;
+            return false;
+        }else if(fs::create_directory(newpath)){
+            cout << "Directory " << directoryname << " creation successfull!" << endl;
+            return true;
+        }else{
+            cerr << "Error: Directory " << directoryname << " creation failed !" << endl;
+            return false;
+        }
+    }catch (const fs::filesystem_error &error){
+                cerr << "Error: " << error.what() << endl;
+                return false;
+    }
+}
+
+bool FileSystem::changedirectory(const string &newDirectory){
+    try
+    {
+        fs::path currentPath = fs::current_path();
+        fs::path newPath = currentPath / newDirectory;
+
+        if (fs::exists(newPath) && fs::is_directory(newPath))
+        {
+            fs::current_path(newPath);
+            cout << "Changed directory to " << newPath << "\n";
+            return true;
+        }
+        else
+        {
+            cerr << "Error: Directory '" << newDirectory << "' does not exist or is not a directory.\n";
+            return false;
+        }
+    }
+    catch (const fs::filesystem_error &error)
+    {
+        cerr << "Error: " << error.what() << "\n";
+        return false;
+    }
+}
+        
+bool FileSystem::copyfile(const string &source, const string &destination){
+    try
+    {
+        fs::path currentPath = fs::current_path();
+        fs::path sourcePath = currentPath / source;
+        fs::copy(sourcePath, destination, fs::copy_options::recursive | fs::copy_options::overwrite_existing);
+        cout << "File copied from " << source << " to " << destination << "\n";
+        return true;
+    }
+    catch (const fs::filesystem_error &error)
+    {
+        cerr << "Error: " << error.what() << "\n";
+        return false;
+    }
+}
+        
+bool FileSystem::renamefile(const string &currentFile, const string &newFile){
+    try
+    {
+        fs::path currentPath = fs::current_path();
+        fs::path currentFilePath = currentPath / currentFile;
+        fs::path newFilePath = currentPath / newFile;
+        if (fs::exists(newFilePath))
+        {
+            cout << "Error: The file name already exists." << endl;
+            return true;
+        }
+        else
+        {
+            fs::rename(currentFilePath, newFilePath);
+            cout << "File renamed from " << currentFile << " to " << newFile << "\n";
+            return false;
+        }
+    }
+    catch (const fs::filesystem_error &error)
+    {
+        cerr << "Error: " << error.what() << "\n";
+        return false;
+    }
+}
+        
+bool FileSystem::deletefile(const string &filename){
+    try
+    {
+        fs::path currentPath = fs::current_path();
+        fs::path filePath = currentPath / filename;
+
+        if (fs::exists(filePath) && fs::is_directory(filePath))
+        {
+            fs::remove_all(filename);
+            cout << "Directory '" << filename << "' and all its contents deleted successfully.\n";
+            return true;
+        }else if(fs::exists(filePath) && !fs::is_directory(filePath))
+        {
+            cout << "File '" << filename << "' deleted successfully.\n";
+            return true;
+        }
+        else
+        {
+            cerr << "Error: File or Directory cannot be deleted.\n";
+            return false;
+        }
+    }
+    catch (const fs::filesystem_error &error)
+    {
+        cerr << "Error: " << error.what() << "\n";
+        return false;
+    }
+}
+        
+void FileSystem::writetofile(const string &filename, const string &content){
+    ofstream file(filename);
+    if (file.is_open())
+    {
+        file << content;
+        file.close();
+        cout << "Content is successfully written on the file " << filename << "!" << endl;
+    }
+    else
+    {
+        cerr << "Unable to create file '" << filename << "' for writing.\n";
+    }
+}
+        
+bool FileSystem::findfile(const string &filename){
+    try {
+        fs::path currentPath = fs::current_path();
+
+        for (const auto& entry : fs::recursive_directory_iterator(currentPath)) {
+            if (entry.path().filename() == filename) {
+                if (fs::is_directory(entry.path())) {
+                    std::cout << "The directory '" << filename << "' is present at '" << entry.path() << "'" << std::endl;
+                    return true;
+                } else {
+                    std::cout << "The file '" << filename << "' is present at '" << entry.path() << "'" << std::endl;
+                    return true;
+                }
+            }else{
+                cerr << "The file '" << filename << "' is not found\n";
+                return false;
+            }
+        }
+    } catch (const fs::filesystem_error& error) {
+        std::cerr << "Error: " << error.what() << "\n";
+        return false;
+    }
+}
+        
+void FileSystem::listdirectory(const string &directory){
+    cout << "Listing contents of directory: "<< "\n";
+    for (const auto &entry : fs::directory_iterator(directory))
+    {
+        cout << entry.path().filename() << "\n";
+    }
+}
+        
+void FileSystem::movetoparentdirectory(const string &directory){
+    fs::path path(directory);
+    fs::path newpath = path.parent_path();
+    fs::current_path(newpath);
+    cout << "Moved up to directory: " << newpath << "\n";
+}
---
+++ Project.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+#include <string>
+#include "FileManager.h"
+#include "Display.h"
+#include "Customize.h"
+using namespace std;
+
+int main(){
+    system("cls");
+    obj_disp.display();
+    string current_directory = obj_disp.get_directory();
+
+    FileManager obj_FM;
+    string user_input;
+    while(true){
+        cout <<"\n"<< current_directory << " >> ";
+        getline(cin, user_input);
+
+        istringstream iss(user_input);
+        string command;
+        iss >> command;
+
+        if (command == obj_disp.get_exit_cmd()){
+            break;
+        }else if (command == "help"){
+            cout << "Available Commands:\n\n";
+            cout << obj_disp.get_customize_cmd()<<" - customize a command keyword\n";
+            cout << obj_disp.get_mkdir_cmd()<<" [directory_name] - Create directory\n";
+            cout << obj_disp.get_read_cmd()<<" [file_name.ext] - read file\n";
+            cout << obj_disp.get_write_cmd()<<" [file_name.ext] - write to a file\n";
+            cout << obj_disp.get_ls_cmd()<<" - List Directory\n";
+            cout << obj_disp.get_ver_cmd()<<" - Version Info\n";
+            cout << obj_disp.get_parentdir_cmd()<<" - Move to Parent Directory\n";
+            cout << obj_disp.get_cd_cmd()<<" [directory_name] - Change directory\n";
+            cout << obj_disp.get_ren_cmd()<<" [file_name] [new_file_name] - Rename File\n";
+            cout << obj_disp.get_copy_cmd()<<" [file_name] [destination] - Copy File\n";
+            cout << obj_disp.get_find_cmd()<<" [file_Name] - Find File In current Directory\n";
+            cout << obj_disp.get_delete_cmd()<<" [file_name] - Delete file\n";
+            cout << obj_disp.get_exit_cmd()<<" - Exit CLI\n";
+        }else if(command == obj_disp.get_copy_cmd()){
+            string source, destination;
+            iss >> source >> destination;
+            obj_FM.execute_command(new CopyFileCommand(source, destination));
+        }else if(command == obj_disp.get_write_cmd()){
+            string fileName,data;
+            getline(iss >> ws, fileName);
+            cout << "Content of file: ";
+            getline(cin, data);
+            obj_FM.execute_command(new WriteToFileCommand(fileName,data));
+        }else if(command == obj_disp.get_delete_cmd()){
+            string fileName;
+            getline(iss >> ws, fileName);
+            obj_FM.execute_command(new DeleteFileCommand(fileName));
+        }else if(command == obj_disp.get_find_cmd()){
+            string fileName;
+            getline(iss >> ws, fileName);
+            obj_FM.execute_command(new FindFileCommand(fileName));
+        }else if(command == obj_disp.get_ren_cmd()){
+            string filename, new_file_name;
+            iss >> filename >> new_file_name;
+            obj_FM.execute_command(new RenameFileCommand(filename,new_file_name));
+        }else if(command == obj_disp.get_cd_cmd()){
+            string directoryName;
+            getline(iss >> ws, directoryName);
+            obj_FM.execute_command(new ChangeDirectoryCommand(directoryName));
+            current_directory = fs::current_path().string();
+        }else if(command == obj_disp.get_parentdir_cmd()){
+            obj_FM.execute_command(new MoveToParentDirectoryCommand(current_directory));
+            current_directory = fs::current_path().string();
+        }else if(command == obj_disp.get_ver_cmd()){
+            cout<<"\n\t (c)SN_TECHNOLOGIES      [ Version 4.1.3 ]\n";
+        }else if(command == obj_disp.get_ls_cmd()){
+            cout << "\n\tDirectory: " << current_directory << "\n\n";
+            obj_FM.execute_command(new ListDirectoryCommand(current_directory));
+        }else if(command == obj_disp.get_read_cmd()){
+            string fileName;
+            getline(iss >> ws, fileName);
+            obj_FM.execute_command(new ReadFileCommand(fileName));
+        }else if(command == obj_disp.get_customize_cmd()){
+            Customize obj_cstmize;
+            obj_cstmize.customize();
+        }else if(command == obj_disp.get_mkdir_cmd()){
+            string directoryName;
+            getline(iss >> ws, directoryName);
+            obj_FM.execute_command(new MakeDirectoryCommand(directoryName));
+        }else{
+            cerr << "Command not recognized! Type 'help' for available commands" << endl;
+        }
+    }
+
+    return 0;
+}
